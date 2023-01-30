0e448724dc52bcdc2c8508e288e231001193c945
brooke_zb <brooke_zb@qq.com>
2023-01-30 22:55:31 +0800

    add sensitive word detect

---
+++ bhs-app/src/main/java/com/brookezb/bhs/app/config/SensitiveWordConfiguration.java
@@ -0,0 +1,19 @@
+package com.brookezb.bhs.app.config;
+
+import com.github.houbb.sensitive.word.bs.SensitiveWordBs;
+import io.quarkus.runtime.Startup;
+
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ * @author brooke_zb
+ */
+public class SensitiveWordConfiguration {
+    @Startup
+    @ApplicationScoped
+    public SensitiveWordBs getSensitiveWords() {
+        return SensitiveWordBs.newInstance()
+                .enableEmailCheck(false)
+                .init();
+    }
+}
